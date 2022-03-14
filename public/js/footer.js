d8587cba7076fcd1fc39b905ceee2b496efa760f
nishagulia <91174692+nishagulia@users.noreply.github.com>
2022-03-14 22:13:03 +0530

    Add files via upload

---
+++ public/js/footer.js
@@ -0,0 +1,56 @@
+const createFooter = () => {
+  let footer = document.querySelector('footer');
+
+  footer.innerHTML = `
+  <div class="footer-content">
+    <img src="../img/light-logo.png" class="logo" alt="">
+    <div class="footer-ul-container">
+      <ul class="category">
+        <li class="category-title">men</li>
+        <li><a href="/search/t-shirt" class="footer-link">t-shirts</a></li>
+        <li><a href="/search/sweatshirt" class="footer-link">sweatshirts</a></li>
+        <li><a href="/search/shirts" class="footer-link">shirts</a></li>
+        <li><a href="/search/jeans" class="footer-link">jeans</a></li>
+        <li><a href="/search/trousers" class="footer-link">trousers</a></li>
+        <li><a href="/search/shoes" class="footer-link">shoes</a></li>
+        <li><a href="/search/casuals" class="footer-link">casuals</a></li>
+        <li><a href="/search/formals" class="footer-link">formals</a></li>
+        <li><a href="/search/sports" class="footer-link">sports</a></li>
+        <li><a href="/search/watch" class="footer-link">watch</a></li>
+      </ul>
+      <ul class="category">
+        <li class="category-title">women</li>
+        <li><a href="/search/t-shirts" class="footer-link">t-shirts</a></li>
+        <li><a href="/search/sweatshirts" class="footer-link">sweatshirts</a></li>
+        <li><a href="/search/shirts" class="footer-link">shirts</a></li>
+        <li><a href="/search/jeans" class="footer-link">jeans</a></li>
+        <li><a href="/search/trousers" class="footer-link">trousers</a></li>
+        <li><a href="/search/shoes" class="footer-link">shoes</a></li>
+        <li><a href="/search/casuals" class="footer-link">casuals</a></li>
+        <li><a href="/search/formals" class="footer-link">formals</a></li>
+        <li><a href="/search/sports" class="footer-link">sports</a></li>
+        <li><a href="/search/watch" class="footer-link">watch</a></li>
+      </ul>
+    </div>
+    </div>
+    <p class="footer-title">about company</p>
+    <p class="info">hlo this is web site cloothing ern]b site is this u want to buy any thing form here this is nest place best quality u get here his is web site cloothing ern]b site is this u want to buy any thing form here this is nest place best quality u get here his is web site cloothing ern]b site is this u want to buy any thing form here this is nest place best quality u get here his is web site cloothing ern]b site is this u want to buy any thing form here this is nest place best quality u get here his is web site cloothing ern]b site is this u want to buy any thing form here this is nest place best quality u get here his is web site cloothing ern]b site is this u want to buy any thing form here this is nest place best quality u get here</p>
+    <p class="info">support emails - help@clothing.com, customer@clothing.com</p>
+    <p class="info">telephone - 180 00 00 001, 180 00 00 002</p>
+    <div class="footer-social-container">
+      <div class="">
+        <a href="#" class="social-link">terms & services</a>
+        <a href="#" class="social-link">privacy page</a>
+      </div>
+      <div class="">
+        <a href="https://www.instagram.com/" class="social-link">instagram</a>
+        <a href="https://www.facebook.com/" class="social-link">facebook</a>
+        <a href="https://www.twitter.com/" class="social-link">twitter</a>
+      </div>
+    </div>
+    <p class="footer-credit">clothing, best apparels online store</p>
+
+  `;
+}
+
+createFooter();
